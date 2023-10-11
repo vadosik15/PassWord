3b7b85d5776bc86b4b45e84557d947ad33575cc9
Vadim Bratus <60158880+vadosik15@users.noreply.github.com>
2023-10-11 15:35:10 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../style.css']
 })
+
 export class AppComponent {
-  title = 'PasswordProject';
+  title = "PasswordProject";
+  password: string = '';
+  isWeak: boolean = false;
+  isMedium: boolean = false;
+  isStrong: boolean = false;
+  isAny: boolean = true;
+
+  checkPassword() {
+    if (!this.password) {
+      this.isAny = true;
+      this.isWeak = false;
+      this.isMedium = false;
+      this.isStrong = false;
+    } else if (this.password.length < 8) {
+      this.isAny = false;
+      this.isWeak = true;
+      this.isMedium = false;
+      this.isStrong = false;
+    } else if (/^[a-zA-Z0-9]+$/.test(this.password)) {
+      this.isAny = false;
+      this.isWeak = false;
+      this.isMedium = true;
+      this.isStrong = false;
+    } else {
+      this.isAny = false;
+      this.isWeak = false;
+      this.isMedium = false;
+      this.isStrong = true;
+    }
+  }
 }
